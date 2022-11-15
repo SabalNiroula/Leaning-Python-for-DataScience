92ab0e73e5f2ef17a76be9a61ea0da43131e9411
Prakash Niroula <sabalniroula5555@gmail.com>
2022-11-15 22:37:30 +0545

    new file was created

---
+++ Practice/oops2.py
@@ -0,0 +1,21 @@
+class Sample:
+
+   x = 10
+
+   @classmethod
+   def modify(change):
+    change.x+=10
+
+    def display():
+        print("value of x", x)
+
+s1 = Sample()
+s2 = Sample()
+
+print("value of x", s1.x)
+print("value of x", s2.x)
+
+
+s1.modify()
+print("value of x", s1.x)
+print("value of x", s2.x)
