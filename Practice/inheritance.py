bd3b11711e9e4d5211f0fa866431ffc995cb3e68
Prakash Niroula <sabalniroula5555@gmail.com>
2022-11-17 18:03:54 +0545

    new file added

--- Practice/inheritance.py
+++ Practice/inheritance.py
@@ -1,43 +1,19 @@
 class Teacher:
+    def __init__(self):
+        self.name1 = "harihar pandey"
 
-    def setName(self, name):
-        self.name = name
-
-    def getName(self):
-        return self.name 
-
-    def setId(self, id):
-        self.id = id
-
-    def getId(self):
-        return self.id
-
-    def setAddress(self, address):
-        self.address = address
-
-    def getAddress(self):
-        return self.address
-    
-    def setSalary(self, salary):
-        self.salary = salary
-
-    def getSalary(self):
-        return self.salary
-
+    def display(self):
+        print("the name is", self.name1)
 
 class Student(Teacher):
+    def __init__(self):
+        super().__init__()
+        super().display()
 
-    def setMarks(self, marks):
-        self.marks = marks
+        self.name = "shyamlal pandit"
 
-    def getMarks(self):
-        return self.marks
-
+    def display(self):
+        print("the name is", self.name)
 
 s = Student()
-s.setName("Sabal Niroula")
-s.setId("2020116334")
-s.setMarks(300)
-s.setAddress("SundarHaraicha -7")
-
-print(s.getName(), s.getAddress(), s.getId(), s.getMarks())
+s.display()
