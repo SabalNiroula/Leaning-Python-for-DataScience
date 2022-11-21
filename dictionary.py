55f4ddeb3c58ce93e365a1026fcf0ee173f98b95
Prakash Niroula <sabalniroula5555@gmail.com>
2022-11-21 17:09:44 +0545

    new file dictionary created

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+x = {}
+
+n = int(input("How many players ? "))
+
+for i in range(n):
+    name = input("Enter player name: ")
+    run = int(input("Enter runs: "))
+    x.update({name:run})
+
+print("\nThe player in the team")
+for k in x.keys():
+    print(k)
+
+name = input("Enter name to get runs: ")
+run = x.get(name, -1)
+if (run == -1):
+    print
