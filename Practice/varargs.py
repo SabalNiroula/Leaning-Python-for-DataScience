2b4e05e95cb1349626d3dc14575337e6c4f29ae4
Prakash Niroula <sabalniroula5555@gmail.com>
2022-11-22 19:57:53 +0545

    practicing variable arguments

---
+++ Practice/varargs.py
@@ -0,0 +1,9 @@
+#this is example of using varargs
+
+def display(var, **kargs):
+    print("formal argument:", var)
+
+    for x,y in kargs.items():
+        print("key = {}, value = {}".format(x, y))
+
+display(10, name= "sabal niroula")
