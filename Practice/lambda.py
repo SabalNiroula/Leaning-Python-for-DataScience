8a7b21d5bc5630899f844cd5d6b3818765f1d338
Prakash Niroula <sabalniroula5555@gmail.com>
2022-11-23 14:28:38 +0545

    filter function practicing

--- Practice/lambda.py
+++ Practice/lambda.py
@@ -1,4 +1,4 @@
-list2 = [1, 34, 45, 44, 67, 76, 23]
+lst = [1, 34, 45, 44, 67, 76, 23]
 
-lst = list(map(lambda x: x*2 if (x%2==0) else x*3, list2))
-print(lst)
+lst1 = list((filter(lambda x: x>4 and x%2==0, lst)))
+print(lst1)
