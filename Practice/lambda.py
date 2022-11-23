643eff34ad3d579ed3cd3686eb25d4468caf2dae
Prakash Niroula <sabalniroula5555@gmail.com>
2022-11-23 14:27:05 +0545

    map function practice

--- Practice/lambda.py
+++ Practice/lambda.py
@@ -1,9 +1,4 @@
-#before lambda we do like this
-def square(a, b):
-    return a+b;
+list2 = [1, 34, 45, 44, 67, 76, 23]
 
-
-sq = lambda x: "Even" if x % 2 == 0 else "Odd"
-
-print(sq(10))
-print(sq(20))
+lst = list(map(lambda x: x*2 if (x%2==0) else x*3, list2))
+print(lst)
